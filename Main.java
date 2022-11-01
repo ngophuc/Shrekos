6d8bace471dfbc99480bb96ea2ad6f17f7081400
Thomas Puibusque <thomaspuibusque@macbook-pro-de-thomas.home>
2022-11-01 15:20:04 +0100

    main sprint 1 commenté

--- Main.java
+++ Main.java
@@ -6,10 +6,11 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		
+		// initialisation de la taille du plateau
 		int longeur=12;
 		int hauteur=12;
 		
+		// initialisation des objets plateau, monstre, tresor et de leurs coord
 		Plateau p = new Plateau(longeur, hauteur);
 		
 		ArrayList<Integer> coordTresor = new ArrayList<Integer>();
@@ -23,13 +24,17 @@ public class Main {
 		ArrayList<Integer> coordHeros = new ArrayList<Integer>();
 		Hero h = new Hero(coordHeros,1,1);
 		h.positionAleatoire(longeur, hauteur, p.getPlateau());
-	
+		
+		// on peut jouer tant que le hero a de la vie et qu'il n'est pas sur la case tresor
 		while (h.getVie()!=0 && !(t.getCoord().equals(h.getCoord()))) {
+			// affichage des coord
 			System.out.println("Heros : \t"+h.getCoord());
 			System.out.println("Monstre : \t"+m.getCoord());
 			System.out.println("Tresor : \t"+t.getCoord());
+			// modification des coord de monstre et hero
 			m.deplacementMonstre(p.getPlateau());
 			h.deplacementHero(p.getPlateau());
+			// si les coord de hero sont les memes que monstre alors hero perd de la vie
 			if (m.getCoord().equals(h.getCoord())) {
 				h.setVie(h.getVie()-m.getAttaque());
 				System.out.println("Aie ta vie : \t"+h.getVie());
@@ -39,11 +44,13 @@ public class Main {
 		System.out.println("Heros : \t"+h.getCoord());
 		System.out.println("Monstre : \t"+m.getCoord());
 		System.out.println("Tresor : \t"+t.getCoord());
-				
+		
+		// annonce si le hero n'a plus de vie et qu'il a donc perdu
 		if (h.getVie()==0) {
 			System.out.println("GAME OVER");
 		}
 		
+		// annonce si le hero a les memes coord que tresor et qu'il a donc gagne
 		if (t.getCoord().equals(h.getCoord())) {
 			System.out.println("WIN");
 		}
